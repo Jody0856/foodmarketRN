734bd43bd23361ad216876bfa6e9e64a75fca09e
jody0856 <jody0856@gmail.com>
2023-01-05 15:22:34 +0700

    update"

--- src/components/molecules/ItemValue/index.js
+++ src/components/molecules/ItemValue/index.js
@@ -1,11 +1,15 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-
-const ItemValue = ({label, value, valueColor = '#020202'}) => {
+import {Number} from '../../../components';
+const ItemValue = ({label, value, valueColor = '#020202', type}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{label}</Text>
-      <Text style={styles.value(valueColor)}>{value}</Text>
+      {type === 'currency' ? (
+        <Number number={value} style={styles.value(valueColor)} />
+      ) : (
+        <Text style={styles.value(valueColor)}>{value}</Text>
+      )}
     </View>
   );
 };
